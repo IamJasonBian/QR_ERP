58dca06bf0841e2eaba200c294d866c8017f750f
Jason Bian <jasonzb@umich.edu>
2020-12-19 15:59:31 -0800

    Update dbo.Increment_Component.sql
    Updated Method

--- Stored Procedures/dbo.Increment_Component.sql
+++ Stored Procedures/dbo.Increment_Component.sql
@@ -1,19 +1,30 @@
-SET QUOTED_IDENTIFIER ON
-GO
+/****** Object:  StoredProcedure [dbo].[Send_proc]    Script Date: 12/19/2020 3:59:04 PM ******/
 SET ANSI_NULLS ON
 GO
-CREATE PROCEDURE [dbo].[Increment_Component]
-    (
-    @Compid int 
-    )
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[Send_proc](
+@Count int,
+@ID int,
+@add_true bit
+)
 AS
 BEGIN 
 SET NOCOUNT ON
 
-UPDATE Components
-SET Quantity = Quantity + 1 
-Where [Component ID] = @Compid
+	declare @_existing_quantity int = (select Qty from INVENTORY where [Inventory ID]=@ID)
+
+	if(@_existing_quantity is not null)
+	begin 
+		if(@add_true=1)
+			begin 
+				update INVENTORY set Qty = @_existing_quantity-@Count where [Inventory ID]=@ID
+			end
+		if(@add_true=0)
+			begin 
+				update INVENTORY set Qty = @_existing_quantity+@Count where [Inventory ID]=@ID
+			end
+	end
 
 END
 
-GO
