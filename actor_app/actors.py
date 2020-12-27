68c63b6793f9f66eda070ded991248d0a40d7f35
Jason Bian <jasonzb@umich.edu>
2020-12-27 00:27:07 -0500

    Merged Both Databases for Component and Inventory

--- actor_app/actors.py
+++ actor_app/actors.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import SubmitField, SelectField
+
 
 from modules import get_names, get_actor, get_id
 import pyodbc
@@ -20,10 +20,6 @@ Bootstrap(app)
 # with Flask-WTF, each web form is represented by a class
 # "NameForm" can change; "(FlaskForm)" cannot
 # see the route for "/" and "index.html" to see how this is used
-class NameForm(FlaskForm):
-    name = StringField('Possible Components are listed below', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-
 
 # all Flask routes below
 
@@ -32,18 +28,21 @@ def index():
     
     #QR Codes goes here
     
-      
+    print("Updated")
+    
     #Write SQL database into json location
     server = 'qrinventory.database.windows.net' 
     database = 'Main' 
     username = 'qr@umich.edu@qrinventory' 
     password = 'ScrambledEggs73' 
     
+  
     cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
     cursor = cnxn.cursor().execute("Select * from dbo.Build")
     columns = [column[0] for column in cursor.description]
-    print(columns)
     
+    
+    #Write local validation code
     results = []
     for row in cursor.fetchall():
         results.append(dict(zip(columns, row)))
@@ -56,45 +55,101 @@ def index():
             f.seek(0,0)
             f.write(line.rstrip('\r') + content)
     
-    with open("sql_data.py", "w") as write_file:
+    with open("build_data.py", "w") as write_file:
         json.dump(results, write_file)
         
-    line_prepender("sql_data.py", "DB = ")
+    line_prepender("build_data.py", "DB = ")
+    
+    
+    
+    
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor().execute("Select * from dbo.Components")
+    columns = [column[0] for column in cursor.description]
+    
+    
+    #Write local validation code
+    results = []
+    for row in cursor.fetchall():
+        results.append(dict(zip(columns, row)))
+    
+    print(results)
+    
+    def line_prepender(filename, line):
+        with open(filename, 'r+') as f:
+            content = f.read()
+            f.seek(0,0)
+            f.write(line.rstrip('\r') + content)
+    
+    with open("component_data.py", "w") as write_file:
+        json.dump(results, write_file)
+        
+    line_prepender("component_data.py", "DB = ")
+    
+   
+    
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor().execute("Select Name from dbo.Build")
+    selection = []
+    for row in cursor.fetchall():
+        selection.append(row[0])
+    
+    
+    selection_2 = []
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor().execute("Select Name from dbo.Components")
+    for row in cursor.fetchall():
+        selection_2.append(row[0])
+        
+    
+    columns = selection + selection_2
+    print(columns)
+   
+   
 
-    from sql_data import DB
+    from build_data import DB as Build_DB
+    from component_data import DB as Comp_DB
     
-    names = get_names(DB)
+    names = columns
+
     
+    class NameForm(FlaskForm):
+        name = SelectField(label = 'Possible Builds or Components are listed below', choices=columns)
+        submit = SubmitField('Submit')
     # you must tell the variable 'form' what you named the class, above
     # 'form' is the variable name used in this template: index.html
     form = NameForm()
     message = ""
     if form.validate_on_submit():
         name = form.name.data
-        if name.lower() in names:
-            # empty the form field
-            form.name.data = ""
-            id = get_id(DB, name)
-            # redirect the browser to another route and template
-            return redirect( url_for('module', id=id) )
+        
+        if name in names:
+            return redirect( url_for('module', name=name))
         else:
             message = "Build or Component does not exist in database."
     return render_template('index.html', names=names, form=form, message=message)
 
-@app.route('/module/<id>')
-def module(id):
-    from sql_data import DB
+@app.route('/module/<name>')
+def module(name, type):
+    from build_data import DB as Build_DB
+    from component_data import DB as Comp_DB
+    
     # run function to get actor data based on the id in the path
     
-    img_base = r'file:///C:\\Users\\jabian\\Desktop\\qr_app\\actor_app\\' 
+    img_base = "/static/"
     
-    id, name, qr = get_actor(DB, id)
-    if name == "Unknown":
+    if(type == "Component"):
+        id, name, qr = get_actor(Comp_DB, name, "Component")
+    elif(type == "Inventory"):
+        id, name, qr = get_actor(Build_DB, name, "Inventory")
+    elif name == "Unknown":
+        
         # redirect the browser to the error template
         return render_template('404.html'), 404
     else:
         # pass all the data for the selected actor to the template
         return render_template('actor.html', id=id, name=name, qr= img_base + qr)
+    
 
 # 2 routes to handle errors - they have templates too
 
--- actor_app/modules.py
+++ actor_app/modules.py
@@ -10,38 +10,69 @@ def get_names(source):
     return sorted(names)
 
 # find the row that matches the id in the URL, retrieve name and photo
-def get_actor(source, id):
+def get_actor(source, id, type):
     import qrcode
     
-    for row in source:
-        if id == str( row["BOM ID"] ):
+    print(type)
+    
+    if type == "Component":
+        
+        for row in source:
             name = row["Name"]
-
             #Generate QR Code, save it, and return the location of the qr
-            qr = "qr_" +  id + ".jpg"
+            qr = "qr_Component" +  id + ".jpg"
             
             #QR code is generated as the page of the webpage
             path =  '/module/' + id
             img = qrcode.make(path)
             
-            img.save("qr_" +  id + ".jpg")
+            img.save("qr_Inventory" +  id + ".jpg")
             
             # change number to string
             id = str(id)
             # return these if id is valid
             return id, name, qr
+    elif type == "Inventory":
+        
+        for row in source:
+            if id == str( row["BOM ID"] ):
+                name = row["Name"]
+    
+                #Generate QR Code, save it, and return the location of the qr
+                qr = "qr_" +  id + ".jpg"
+                
+                #QR code is generated as the page of the webpage
+                path =  '/module/' + id
+                img = qrcode.make(path)
+                
+                img.save("qr_Component" +  id + ".jpg")
+                
+                # change number to string
+                id = str(id)
+                # return these if id is valid
+                return id, name, qr
     # return these if id is not valid - not a great solution, but simple
     return "Unknown", "Unknown", ""
 
 # find the row that matches the name in the form and retrieve matching id
-def get_id(source, name):
-    for row in source:
-        # lower() makes the string all lowercase
-        if name.lower() == row["Name"].lower():
-            id = row["BOM ID"]
-            # change number to string
-            id = str(id)
-            # return id if name is valid
-            return id
+def get_id(source, name, type):
+    if(type == "Component"):
+         for row in source:
+            # lower() makes the string all lowercase
+            if name.lower() == row["Name"].lower():
+                id = row["Component ID"]
+                # change number to string
+                id = str(id)
+                # return id if name is valid
+                return id
+    else:
+        for row in source:
+            # lower() makes the string all lowercase
+            if name.lower() == row["Name"].lower():
+                id = row["BOM ID"]
+                # change number to string
+                id = str(id)
+                # return id if name is valid
+                return id
     # return these if id is not valid - not a great solution, but simple
     return "Unknown"
