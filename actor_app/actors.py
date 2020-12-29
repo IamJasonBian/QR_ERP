be448b7a32287421accbe211b3abdf905289c3f9
Jason Bian <jasonzb@umich.edu>
2020-12-29 17:59:14 -0500

    Finished MVP, Send functionality does not work yet

--- actor_app/actors.py
+++ actor_app/actors.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, SelectField
+from modules import gen_py_db
+import pandas as pd
 
+from modules import gen_qr, exe_py_proc, get_id
 
-from modules import get_names, get_actor, get_id
-import pyodbc
 import json
 import qrcode
+import pyodbc
+
+
+
+
 
 app = Flask(__name__)
 
@@ -23,134 +29,129 @@ Bootstrap(app)
 
 # all Flask routes below
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     
-    #QR Codes goes here
     
-    print("Updated")
+        if request.method == "POST":
+            #On Form Submit
+            req = request.form
     
-    #Write SQL database into json location
-    server = 'qrinventory.database.windows.net' 
-    database = 'Main' 
-    username = 'qr@umich.edu@qrinventory' 
-    password = 'ScrambledEggs73' 
-    
-  
-    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-    cursor = cnxn.cursor().execute("Select * from dbo.Build")
-    columns = [column[0] for column in cursor.description]
-    
-    
-    #Write local validation code
-    results = []
-    for row in cursor.fetchall():
-        results.append(dict(zip(columns, row)))
-    
-    print(results)
-    
-    def line_prepender(filename, line):
-        with open(filename, 'r+') as f:
-            content = f.read()
-            f.seek(0,0)
-            f.write(line.rstrip('\r') + content)
-    
-    with open("build_data.py", "w") as write_file:
-        json.dump(results, write_file)
-        
-    line_prepender("build_data.py", "DB = ")
-    
-    
-    
-    
-    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-    cursor = cnxn.cursor().execute("Select * from dbo.Components")
-    columns = [column[0] for column in cursor.description]
-    
-    
-    #Write local validation code
-    results = []
-    for row in cursor.fetchall():
-        results.append(dict(zip(columns, row)))
-    
-    print(results)
-    
-    def line_prepender(filename, line):
-        with open(filename, 'r+') as f:
-            content = f.read()
-            f.seek(0,0)
-            f.write(line.rstrip('\r') + content)
-    
-    with open("component_data.py", "w") as write_file:
-        json.dump(results, write_file)
-        
-    line_prepender("component_data.py", "DB = ")
-    
-   
-    
-    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-    cursor = cnxn.cursor().execute("Select Name from dbo.Build")
-    selection = []
-    for row in cursor.fetchall():
-        selection.append(row[0])
-    
-    
-    selection_2 = []
-    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-    cursor = cnxn.cursor().execute("Select Name from dbo.Components")
-    for row in cursor.fetchall():
-        selection_2.append(row[0])
-        
-    
-    columns = selection + selection_2
-    print(columns)
-   
-   
+            Quantity = req.get("Quantity")
+            direction = req.get("direction")
+            
+            if(direction == "+"):
+                direction = "1"
+            else:
+                direction = "0"
+            
+            Build_Send = req.get("Build_Send")
+            Warranty_ID = req.get("Warranty ID")
+            name = req.get("Item_name")
+            Type = req.get("type")
+            
+            ID = str(get_id(name, Type))
+            
+            print(Quantity)
+            print(direction)
+            print(Build_Send)
+            print(Warranty_ID)
+            print(name)
+            print(Type)
+            print(ID)
+            
+            Quantity= Quantity
+            direction = direction
+            
+            #Need to figure this out later
+            Warranty_ID = Warranty_ID
+            
+            exe_py_proc(Type, ID, Quantity, direction, Build_Send)
+            #Trigger the stored procs
 
-    from build_data import DB as Build_DB
-    from component_data import DB as Comp_DB
+        #Gen build py script
+        gen_py_db("Select * from dbo.Build", "build_data.py", "DB_write")
+        
+        #Gen component py script
+        gen_py_db("Select * from dbo.Components", "component_data.py", "DB_write")
+        
+        
+        Build_name = gen_py_db("Select * from dbo.Build", "build_data.py", "Get_Name")
+        #Generate build label
+        Type_1 = ["Inventory"] * len(Build_name)
+        
+        Component_name = gen_py_db("Select * from dbo.Components", "component_data.py", "Get_Name")
+        #Generate component label
+        Type_2 = ["Component"] * len(Component_name)
+        
+        names = Build_name + Component_name
+        types = Type_1 + Type_2
+        
+        dropdown_df = pd.DataFrame(list(zip(names, types)), 
+                   columns =['names', 'types']) 
     
-    names = columns
+        
+        class NameForm(FlaskForm):
+            name = SelectField(label = 'Possible Builds or Components are listed below', choices=dropdown_df['names'])
+            submit = SubmitField('Submit')
+        # you must tell the variable 'form' what you named the class, above
+        # 'form' is the variable name used in this template: index.html
+        form = NameForm()
+        message = ""
+        if form.validate_on_submit():
+            name = form.name.data
+            
+            print(dropdown_df[dropdown_df['types'] == 'Component']['names'].tolist())
+            print(dropdown_df[dropdown_df['types'] == 'Inventory']['names'].tolist())
+            
+            
+            if name in dropdown_df[dropdown_df['types'] == 'Component']['names'].tolist():
+                return redirect(url_for('input', name=name, type="Component"))
+            elif name in dropdown_df[dropdown_df['types'] == 'Inventory']['names'].tolist():
+                return redirect(url_for('input', name=name, type="Inventory"))
+            else:
+                message = "Build or Component does not exist in database."
+        return render_template('index.html', names=names, form=form, message=message)
 
     
-    class NameForm(FlaskForm):
-        name = SelectField(label = 'Possible Builds or Components are listed below', choices=columns)
-        submit = SubmitField('Submit')
-    # you must tell the variable 'form' what you named the class, above
-    # 'form' is the variable name used in this template: index.html
-    form = NameForm()
-    message = ""
-    if form.validate_on_submit():
-        name = form.name.data
-        
-        if name in names:
-            return redirect( url_for('module', name=name))
-        else:
-            message = "Build or Component does not exist in database."
-    return render_template('index.html', names=names, form=form, message=message)
-
-@app.route('/module/<name>')
-def module(name, type):
-    from build_data import DB as Build_DB
-    from component_data import DB as Comp_DB
+@app.route('/input/<name>/<type>', methods=["GET", "POST"])
+def input(name, type):
+    
+    
+    
     
     # run function to get actor data based on the id in the path
-    
-    img_base = "/static/"
-    
-    if(type == "Component"):
-        id, name, qr = get_actor(Comp_DB, name, "Component")
-    elif(type == "Inventory"):
-        id, name, qr = get_actor(Build_DB, name, "Inventory")
-    elif name == "Unknown":
+    if(type == "Inventory"):
+        image_url = "/static/Inventory/"
         
+    elif(type == "Component"):
+        image_url = "/static/Component/"
+       
+
+    #image_url = img_base + name + ".jpg"
+    image_name =  name + ".jpg"
+    
+    path_url = ""
+    
+    print(image_url)
+    
+    gen_qr(image_name, image_url, path_url)
+    
+    if name == "Unknown":
         # redirect the browser to the error template
         return render_template('404.html'), 404
-    else:
+    elif(type == "Inventory"):
         # pass all the data for the selected actor to the template
-        return render_template('actor.html', id=id, name=name, qr= img_base + qr)
+        return render_template('Inventory.html', name=name, qr= image_url + image_name)
+    elif(type == "Component"):
+        # pass all the data for the selected actor to the template
+        return render_template('Component.html', name=name, qr= image_url + image_name)
+    else:
+        # redirect the browser to the error template
+        return render_template('404.html'), 404
     
-
+    
 # 2 routes to handle errors - they have templates too
 
 @app.errorhandler(404)
@@ -160,6 +161,27 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+@app.route('/output', methods=['GET', 'POST'])
+def output():
+        
+    if request.method == "POST":
+            req = request.form
+        
+            Quantity = req.get("Quantity")
+            direction = req.get("direction")
+            Build_Send = req.get("Build_Send")
+            Warranty_ID = req.get("Warranty ID")
+            
+            print(Quantity)
+            print(direction)
+            print(Build_Send)
+            print(Warranty_ID)
+        
+            return redirect(request.url)
+    else:
+        return render_template('index.html'), 404
+    
 
 
 # keep this as is
--- actor_app/modules.py
+++ actor_app/modules.py
@@ -10,69 +10,135 @@ def get_names(source):
     return sorted(names)
 
 # find the row that matches the id in the URL, retrieve name and photo
-def get_actor(source, id, type):
+def gen_qr(image_name, image_url, path_url):
     import qrcode
+    import os
+    from pathlib import Path
     
-    print(type)
-    
-    if type == "Component":
+    img = qrcode.make(image_name)
         
-        for row in source:
-            name = row["Name"]
-            #Generate QR Code, save it, and return the location of the qr
-            qr = "qr_Component" +  id + ".jpg"
-            
-            #QR code is generated as the page of the webpage
-            path =  '/module/' + id
-            img = qrcode.make(path)
-            
-            img.save("qr_Inventory" +  id + ".jpg")
-            
-            # change number to string
-            id = str(id)
-            # return these if id is valid
-            return id, name, qr
-    elif type == "Inventory":
-        
-        for row in source:
-            if id == str( row["BOM ID"] ):
-                name = row["Name"]
+    #add site url here
+    img.save(image_name)
     
-                #Generate QR Code, save it, and return the location of the qr
-                qr = "qr_" +  id + ".jpg"
-                
-                #QR code is generated as the page of the webpage
-                path =  '/module/' + id
-                img = qrcode.make(path)
-                
-                img.save("qr_Component" +  id + ".jpg")
-                
-                # change number to string
-                id = str(id)
-                # return these if id is valid
-                return id, name, qr
+    #Delete QR if exists
+    if os.path.exists("." + image_url + image_name):
+        os.remove("." + image_url + image_name)
+    
+    #Move image to static location
+    Path(image_name).rename("." + image_url + image_name)
+        
     # return these if id is not valid - not a great solution, but simple
-    return "Unknown", "Unknown", ""
+    return "QR_Generated"
 
-# find the row that matches the name in the form and retrieve matching id
-def get_id(source, name, type):
-    if(type == "Component"):
-         for row in source:
-            # lower() makes the string all lowercase
-            if name.lower() == row["Name"].lower():
-                id = row["Component ID"]
-                # change number to string
-                id = str(id)
-                # return id if name is valid
-                return id
+# find QR codes
+def get_id(name, Type):
+    from build_data import DB as build_db
+    from component_data import DB as component_db
+    
+    
+    print("Extracting ID")
+    ID = 0
+    
+    if(Type=="Component"):
+        for row in component_db:
+            if name == row['Name']:
+                ID = row['Component ID']
+    elif(Type=="Inventory"):
+       for row in build_db:
+            if name == row['Name']:
+                ID = row['Inventory ID']
+            
     else:
-        for row in source:
-            # lower() makes the string all lowercase
-            if name.lower() == row["Name"].lower():
-                id = row["BOM ID"]
-                # change number to string
-                id = str(id)
-                # return id if name is valid
-                return id
-    # return these if id is not valid - not a great solution, but simple
-    return "Unknown"
+        print("Type not found")
+        return()
+        
+    return(ID)
+    
+
+
+
+#Used to write local string text files
+def line_prepender(filename, line):
+    with open(filename, 'r+') as f:
+        content = f.read()
+        f.seek(0,0)
+        f.write(line.rstrip('\r') + content)
+
+#Create local .py files
+def gen_py_db(Query, output, Type):
+    import pyodbc
+    import json
+
+    print("Starting Server Connection")
+        
+    #Write SQL database into json location
+    server = 'qrinventory.database.windows.net' 
+    database = 'Main' 
+    username = 'qr@umich.edu@qrinventory' 
+    password = 'ScrambledEggs73' 
+    
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor().execute(Query)
+    
+    
+    if(Type == "DB_write"):
+    
+        columns = [column[0] for column in cursor.description]
+        
+        #Write local validation code
+        results = []
+        for row in cursor.fetchall():
+            results.append(dict(zip(columns, row)))
+           
+        
+        with open(output, "w") as write_file:
+            json.dump(results, write_file)
+            
+        line_prepender(output, "DB = ")
+        
+    elif(Type == "Get_Name"):
+        if (output == "build_data.py"):
+            selection = []
+            for row in cursor.fetchall():
+                selection.append(row[1])
+        elif (output == "component_data.py"):
+            selection = []
+            for row in cursor.fetchall():
+                selection.append(row[0])
+        return(selection)
+        
+    return "done"
+
+def exe_py_proc(Type, ID, Quantity, direction, Build_Send):
+    import pyodbc
+
+    print("Executing Stored Proc")
+        
+    #Write SQL database into json location
+    server = 'qrinventory.database.windows.net' 
+    database = 'Main' 
+    username = 'qr@umich.edu@qrinventory' 
+    password = 'ScrambledEggs73' 
+    
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
+    
+    
+    if(Type=="Inventory"):
+        if(Build_Send=="Build"):
+            cursor.execute("exec Build_proc " + ID + "," + Quantity + "," + direction)
+        elif(Build_Send=="Send"):
+            cursor.execute("exec Send_proc " + ID + "," + Quantity + "," + direction)
+        else:
+            print("Build/Send is non-existant")
+    elif(Type=="Component"):
+        cursor.execute("exec Increment_Component_proc " + ID + "," + Quantity + "," + direction)
+    else:
+        print("Stored Proc Type is non-existant")
+        
+    #Send stored proc execution
+    cnxn.commit()
+    
+    return("Complete")
+    
+    
